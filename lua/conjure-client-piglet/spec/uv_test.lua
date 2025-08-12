f409e57947b24a99d01d1b701ea7095224680954
Laurence Chen <laurence@replware.dev>
2025-08-12 19:03:47 +0800

    update testing files

--- lua/conjure-client-piglet/spec/uv_test.lua
+++ lua/conjure-client-piglet/spec/uv_test.lua
@@ -1,4 +1,7 @@
-local server = require'server_uv'.listen
+-- open this file in Neovim and use command `:luafile %` to invoke it.
+-- In other terminal, run the command `websocat ws://127.0.0.1:8080` to test it
+
+local server = require("conjure-client-piglet.server_uv").listen
 {
   -- listen on port 8080
   port = 8080,
